2f5254f28ec24bf797c5169cf81d10a8037306e4
bhaanukumi <bbhaanureaka@mmhglobal.com>
2022-12-20 22:18:38 +1300

    Adding files for Jenkins Report

--- src/test/java/utilities/CommonUtils.java
+++ src/test/java/utilities/CommonUtils.java
@@ -31,8 +31,14 @@ public class CommonUtils {
 	}
 	
 	//config file loader
-	public void loadProperties() {
+	@Parameters("Browser")
+	public void loadProperties(String strBrowsernameJenkinsParameter) {
 
+
+		System.out.println("Inside loadProperties");
+		System.out.println("Jenkins parameter is: "+strBrowsernameJenkinsParameter);
+
+		System.out.println("URL is: "+Constants.APP_URL);
 		Properties properties =new Properties();
 		try {
 			properties.load(getClass().getResourceAsStream("/config.properties"));
