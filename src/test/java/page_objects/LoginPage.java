1b1c5a533daf1843e8a06aaf6f4560f1f370c85d
bhaanukumi <bbhaanureaka@mmhglobal.com>
2023-01-14 00:06:25 +1300

    adding testng-failed.xml in pom file

--- src/test/java/page_objects/LoginPage.java
+++ src/test/java/page_objects/LoginPage.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-import org.testng.annotations.Test;
 import webdriver_manager.DriverManager;
 
 public class LoginPage {
